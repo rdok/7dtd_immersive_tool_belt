0776daf8b7e4b3c6942ae81814661b78c8c6fe05
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-04 21:44:22 +0100

    chore: Customer logger.

---
+++ ImmersiveToolBelt/Harmony/Logger.cs
@@ -0,0 +1,41 @@
+namespace ImmersiveToolBelt.Harmony
+{
+    public class Logger : ILogger
+    {
+        public void Info(string message)
+        {
+#if DEBUG
+            Log.Out("[ImmersiveToolBelt: " + message);
+#endif
+        }
+
+        public void Warn(string message)
+        {
+#if DEBUG
+            Log.Warning("[ImmersiveToolBelt " + message);
+#endif
+        }
+
+        public void Warning(string message)
+        {
+#if DEBUG
+            Log.Warning("[ImmersiveToolBelt " + message);
+#endif
+        }
+
+        public void Error(string message)
+        {
+#if DEBUG
+            Log.Error("[ImmersiveToolBelt" + message);
+#endif
+        }
+    }
+
+    public interface ILogger
+    {
+        void Info(string message);
+        void Warn(string message);
+        void Warning(string message);
+        void Error(string message);
+    }
+}
