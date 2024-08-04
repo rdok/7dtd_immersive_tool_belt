eed70a7df4e5029b08e0decf0111ed539cb67f33
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-04 21:47:03 +0100

    test: Show tool belt having opened backpack only.

--- UnitTests/Harmony/Factory.cs
+++ UnitTests/Harmony/Factory.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 using ImmersiveToolBelt.Harmony;
-using ImmersiveToolBelt.Harmony.Seams;
+using ImmersiveToolBelt.Harmony.Interfaces;
 using Moq;
 
 namespace UnitTests.Harmony;
@@ -13,24 +13,52 @@ public static class Factory
         var xuiMock = new Mock<IXUi>();
         var playerUIMock = new Mock<ILocalPlayerUI>();
         var entityPlayerLocalMock = new Mock<IEntityPlayerLocal>();
+        var windowManagerMock = new Mock<IGUIWindowManager>();
+        var toolBeltWindowMock = new Mock<IXUiControllerChild>();
+        var viewComponentMock = new Mock<IXUiView>();
+
 
         if (parameters == null)
-        {
             return new FactoryContainer
             {
                 entityPlayerLocalMock = entityPlayerLocalMock,
                 playerUIMock = playerUIMock,
                 xUIMock = xuiMock
             };
-        }
-        
+
         var hasXui = parameters.ContainsKey("hasXui") && (bool)parameters["hasXui"];
         var xui = hasXui ? xuiMock.Object : null;
         playerUIMock.Setup(p => p.xui).Returns(xui);
 
-        var hasToolBeltGroup = parameters.ContainsKey("hasToolBeltGroup") && (bool)parameters["hasToolBeltGroup"];
-        var toolBeltGroup = hasToolBeltGroup ? toolBeltWindowGroupMock.Object : null;
-        xuiMock.Setup(p => p.FindWindowGroupByName("toolbelt")).Returns(toolBeltGroup);
+        var hasWindowManager = parameters.ContainsKey("hasWindowManager") && (bool)parameters["hasWindowManager"];
+        var windowManager = hasWindowManager ? windowManagerMock.Object : null;
+        playerUIMock.Setup(p => p.windowManager).Returns(windowManager);
+
+        var isBackpackOpen = parameters.ContainsKey("isBackpackOpen") && (bool)parameters["isBackpackOpen"];
+        windowManagerMock.Setup(p => p.IsWindowOpen("backpack")).Returns(isBackpackOpen);
+
+        var isVisible =
+            parameters.ContainsKey("isVisible") && (bool)parameters["isVisible"];
+        viewComponentMock.Setup(p => p.IsVisible).Returns(isVisible);
+
+        var hasViewComponent =
+            parameters.ContainsKey("hasViewComponent") && (bool)parameters["hasViewComponent"];
+        var viewComponent = hasViewComponent ? viewComponentMock.Object : null;
+        toolBeltWindowMock.Setup(p => p.ViewComponent).Returns(viewComponent);
+
+        var hasToolBeltOpen =
+            parameters.ContainsKey("isToolBeltOpen") && (bool)parameters["isToolBeltOpen"];
+        viewComponentMock.Setup(p => p.IsVisible).Returns(hasToolBeltOpen);
+
+        var hasToolBeltWindow =
+            parameters.ContainsKey("hasToolBeltWindow") && (bool)parameters["hasToolBeltWindow"];
+        var toolBeltWindow = hasToolBeltWindow ? toolBeltWindowMock.Object : null;
+        toolBeltWindowGroupMock.Setup(p => p.GetChildById("windowToolbelt")).Returns(toolBeltWindow);
+
+        var hasToolBeltWindowGroup =
+            parameters.ContainsKey("hasToolBeltWindowGroup") && (bool)parameters["hasToolBeltWindowGroup"];
+        var toolBeltWindowGroup = hasToolBeltWindowGroup ? toolBeltWindowGroupMock.Object : null;
+        xuiMock.Setup(p => p.FindWindowGroupByName("toolbelt")).Returns(toolBeltWindowGroup);
 
         var hasPlayerUI = parameters.ContainsKey("PlayerUI") && (bool)parameters["PlayerUI"];
         var playerUIInstance = hasPlayerUI ? playerUIMock.Object : null;
@@ -50,15 +78,21 @@ public static class Factory
             entityPlayerLocalMock = entityPlayerLocalMock,
             playerUIMock = playerUIMock,
             xUIMock = xuiMock,
-            toolBeltWindowGroupMock = toolBeltWindowGroupMock
+            toolBeltWindowGroupMock = toolBeltWindowGroupMock,
+            toolBeltWindowMock = toolBeltWindowMock,
+            windowManagerMock = windowManagerMock,
+            viewComponentMock = viewComponentMock
         };
     }
 }
 
 public class FactoryContainer
 {
+    public Mock<IXUiControllerChild> toolBeltWindowMock { get; set; }
     public Mock<IXUiController> toolBeltWindowGroupMock { get; set; }
     public Mock<IEntityPlayerLocal> entityPlayerLocalMock { get; set; }
     public Mock<ILocalPlayerUI> playerUIMock { get; set; }
     public Mock<IXUi> xUIMock { get; set; }
+    public Mock<IGUIWindowManager> windowManagerMock { get; set; }
+    public Mock<IXUiView> viewComponentMock { get; set; }
 }
--- UnitTests/Harmony/ImmersiveToolBeltTests.cs
+++ UnitTests/Harmony/ImmersiveToolBeltTests.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using Moq;
+using ImmersiveToolBelt.Harmony;
 using NUnit.Framework;
 
 namespace UnitTests.Harmony;
@@ -8,38 +8,38 @@ namespace UnitTests.Harmony;
 public class ImmersiveToolBeltInitTests
 {
     [Test]
-    public void it_skis_update_having_a_nullable_player_instance()
+    public void it_skips_update_having_no_player_instance()
     {
         var factory = Factory.Create();
-        ImmersiveToolBelt.Harmony.ToolBelt.UpdateVisibility(null);
+        ToolBelt.UpdateVisibility(null);
         factory.entityPlayerLocalMock.VerifyNoOtherCalls();
     }
 
     [Test]
-    public void it_skis_update_having_changed_player_stats()
+    public void it_skips_update_having_changed_player_stats()
     {
         var factory = Factory.Create(new Dictionary<string, object>
         {
             ["bPlayerStatsChanged"] = true
         });
-        ImmersiveToolBelt.Harmony.ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
         factory.playerUIMock.VerifyNoOtherCalls();
     }
 
     [Test]
-    public void it_skis_update_having_a_nullable_player_ui()
+    public void it_skips_update_having_no_player_ui()
     {
         var factory = Factory.Create(new Dictionary<string, object>
         {
             ["bPlayerStatsChanged"] = false,
             ["PlayerUI"] = false
         });
-        ImmersiveToolBelt.Harmony.ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
         factory.xUIMock.VerifyNoOtherCalls();
     }
-    
+
     [Test]
-    public void it_skis_update_having_a_nullable_xui()
+    public void it_skips_update_having_no_xui()
     {
         var factory = Factory.Create(new Dictionary<string, object>
         {
@@ -47,7 +47,93 @@ public class ImmersiveToolBeltInitTests
             ["PlayerUI"] = true,
             ["hasXui"] = false
         });
-        ImmersiveToolBelt.Harmony.ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
-        factory.toolBeltWindowGroupMock.VerifyNoOtherCalls();
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.xUIMock.VerifyNoOtherCalls();
+    }
+
+    [Test]
+    public void it_skips_update_having_no_tool_belt_window_group()
+    {
+        var factory = Factory.Create(new Dictionary<string, object>
+        {
+            ["bPlayerStatsChanged"] = false,
+            ["PlayerUI"] = true,
+            ["hasXui"] = true,
+            ["hasToolBeltWindowGroup"] = true
+        });
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.toolBeltWindowMock.VerifyNoOtherCalls();
+    }
+
+    [Test]
+    public void it_skips_update_having_no_tool_belt_window()
+    {
+        var factory = Factory.Create(new Dictionary<string, object>
+        {
+            ["bPlayerStatsChanged"] = false,
+            ["PlayerUI"] = true,
+            ["hasXui"] = true,
+            ["hasToolBeltWindowGroup"] = true,
+            ["hasWindowManager"] = false
+        });
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.windowManagerMock.VerifyNoOtherCalls();
+    }
+
+    [Test]
+    public void it_checks_if_backpack_is_open()
+    {
+        var factory = Factory.Create(new Dictionary<string, object>
+        {
+            ["bPlayerStatsChanged"] = false,
+            ["PlayerUI"] = true,
+            ["hasXui"] = true,
+            ["hasToolBeltWindowGroup"] = true,
+            ["hasToolBeltWindow"] = true,
+            ["hasWindowManager"] = true,
+            ["hasViewComponent"] = true
+        });
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.windowManagerMock.Verify(h => h.IsWindowOpen("backpack"));
+    }
+
+    [Test]
+    public void it_hides_the_tool_belt_window_having_backpack_open_and_tool_belt_closed()
+    {
+        var factory = Factory.Create(new Dictionary<string, object>
+        {
+            ["bPlayerStatsChanged"] = false,
+            ["PlayerUI"] = true,
+            ["hasXui"] = true,
+            ["hasToolBeltWindowGroup"] = true,
+            ["hasToolBeltWindow"] = true,
+            ["hasWindowManager"] = true,
+            ["hasViewComponent"] = true,
+            ["isBackpackOpen"] = true,
+            ["isToolBeltOpen"] = false
+        });
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.viewComponentMock.VerifySet(h => h.ForceHide = false);
+        factory.viewComponentMock.VerifySet(h => h.IsVisible = true);
+    }
+
+    [Test]
+    public void it_shows_the_tool_belt_window_having_backpack_closed_and_tool_belt_open()
+    {
+        var factory = Factory.Create(new Dictionary<string, object>
+        {
+            ["bPlayerStatsChanged"] = false,
+            ["PlayerUI"] = true,
+            ["hasXui"] = true,
+            ["hasToolBeltWindowGroup"] = true,
+            ["hasToolBeltWindow"] = true,
+            ["hasWindowManager"] = true,
+            ["hasViewComponent"] = true,
+            ["isBackpackOpen"] = false,
+            ["isToolBeltOpen"] = true
+        });
+        ToolBelt.UpdateVisibility(factory.entityPlayerLocalMock.Object);
+        factory.viewComponentMock.VerifySet(h => h.ForceHide = true);
+        factory.viewComponentMock.VerifySet(h => h.IsVisible = false);
     }
 }
