b4ea503b89efe1e0442dc7d0ec393da4a13bb3d6
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-04 17:18:36 +0100

    feat: Initialise mod.

---
+++ ImmersiveToolBelt/Harmony/Main.cs
@@ -0,0 +1,19 @@
+﻿namespace ImmersiveToolBelt.Harmony
+{
+    public class Main
+    {
+        private static readonly ILogger Logger = new Logger();
+
+        public class Init : IModApi
+        {
+            public void InitMod(Mod modInstance)
+            {
+                var type = GetType();
+                var message = type.ToString();
+                Logger.Info("Loading Patch: " + message);
+                var harmony = new HarmonyLib.Harmony(message);
+                harmony.PatchAll();
+            }
+        }
+    }
+}
