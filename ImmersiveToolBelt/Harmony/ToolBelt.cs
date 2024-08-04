8d4ad455ab61f008ccdf39a728032fbaf0840147
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-04 21:51:10 +0100

    feat: Show tool belt window only having opened backpack.

---
+++ ImmersiveToolBelt/Harmony/ToolBelt.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using ImmersiveToolBelt.Harmony.Interfaces;
+using ImmersiveToolBelt.Harmony.Seams;
+
+namespace ImmersiveToolBelt.Harmony
+{
+    [HarmonyPatch(typeof(EntityPlayerLocal), "Update")]
+    public static class ToolBelt
+    {
+        private static ILogger _logger = new Logger();
+
+        public static void SetLogger(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public static void Postfix(EntityPlayerLocal __instance)
+        {
+            var entityPlayerLocalWrapper = new EntityPlayerLocalSeam(__instance);
+            UpdateVisibility(entityPlayerLocalWrapper);
+        }
+
+        public static void UpdateVisibility(IEntityPlayerLocal player)
+        {
+            if (player == null) return;
+            if (player.bPlayerStatsChanged) return;
+
+            var playerUI = player.PlayerUI;
+            if (playerUI == null) return;
+
+            var xui = playerUI.xui; // TODO: repalace withwindow manager check
+            if (xui == null) return;
+
+            var toolBeltWindowGroup = xui.FindWindowGroupByName("toolbelt");
+            if (toolBeltWindowGroup == null) return;
+
+            var toolBeltWindow = toolBeltWindowGroup.GetChildById("windowToolbelt");
+            if (toolBeltWindow == null) return;
+
+            var isBackpackOpen = playerUI.windowManager.IsWindowOpen("backpack");
+            var toolBeltWindowIsOpen = toolBeltWindow.ViewComponent.IsVisible;
+
+            if (isBackpackOpen && !toolBeltWindowIsOpen)
+            {
+                _logger.Info("Inventory open, showing windowToolbelt.");
+                toolBeltWindow.ViewComponent.ForceHide = false;
+                toolBeltWindow.ViewComponent.IsVisible = true;
+            }
+            else if (!isBackpackOpen && toolBeltWindowIsOpen)
+            {
+                _logger.Info("Inventory closed, hiding windowToolbelt.");
+                toolBeltWindow.ViewComponent.ForceHide = true;
+                toolBeltWindow.ViewComponent.IsVisible = false;
+            }
+        }
+    }
+}
